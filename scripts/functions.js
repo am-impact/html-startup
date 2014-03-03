dce92aaa7c73b20931a26ebf4fc754c617268a97
Niek <n.weevers@am-impact.nl>
2014-03-03 19:05:39 +0100

    Retina functie aanpassen, anoniem maken

--- scripts/functions.js
+++ scripts/functions.js
@@ -4,22 +4,25 @@ var FW = FW || {};
 /**
  * Retina images
  */
-if (window.devicePixelRatio == 2) {
+(function() {
+    if (window.devicePixelRatio == 2) {
 
-    var images = $("img[data-retina]");
+        var $images = $("img[data-retina]");
 
-    // loop through the images and make them hi-res
-    for(var i = 0; i < images.length; i++) {
+        // loop through the $images and make them hi-res
+        for( var i = 0; i < $images.length; i++ ) {
 
-        // create new image name
-        var imageType = images[i].src.substr(-4);
-        var imageName = images[i].src.substr(0, images[i].src.length - 4);
-        imageName += "@2x" + imageType;
+            // create new image name
+            var imageType = $images[i].src.substr(-4),
+                imageName = $images[i].src.substr(0, $images[i].src.length - 4);
 
-        //rename image
-        images[i].src = imageName;
+            imageName += "@2x" + imageType;
+
+            // rename image
+            $images[i].src = imageName;
+        }
     }
-}
+})();
 
 
 /**
