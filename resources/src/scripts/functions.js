559ef52e2107301f22df1a48ce8ee5f30475d422
Niek Weevers <niekweevers@gmail.com>
2015-02-05 09:09:53 +0100

    Update startup nav h5bp 5.0.0
    - Console functie update: https://github.com/h5bp/html5-boilerplate/pull/1559
    - doctype met kleine letters: https://github.com/h5bp/html5-boilerplate/issues/1522
    - iframe, svg vertical-align: middle: https://github.com/h5bp/html5-boilerplate/pull/1613
    - Update normalize: https://github.com/h5bp/html5-boilerplate/issues/1050
    - Update print.css: https://github.com/h5bp/html5-boilerplate/pull/1585
    - .ir verwijderen, gebruiken wij ook nooit: https://github.com/h5bp/html5-boilerplate/pull/1475

--- resources/src/scripts/functions.js
+++ resources/src/scripts/functions.js
@@ -59,7 +59,7 @@ var FW = FW || {};
         'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
         'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
         'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
     ];
     var length = methods.length;
     var console = (window.console = window.console || {});
